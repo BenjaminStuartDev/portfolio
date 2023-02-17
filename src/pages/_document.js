82e47ba6b0da9e218e523dc258f3a264257822b1
Benjamin Stuart <ben.stuart@tanda.co>
2023-02-17 13:55:57 +1000

    Fix silly tailwind mistake

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-mine-shaft">
+      <body className="bg-cornflower-blue">
         <Main />
         <NextScript />
       </body>
